08734fa9452b3e233dd45225aef334bb4157cad3
Richard <richardizip@gmail.com>
2019-02-05 10:12:46 +0100

    Setup prettier and eslint

--- graphql-server/src/index.js
+++ graphql-server/src/index.js
@@ -1,18 +1,18 @@
-const { ApolloServer, gql } = require("apollo-server");
+const {ApolloServer, gql} = require('apollo-server')
 
 // This is a (sample) collection of books we'll be able to query
 // the GraphQL server for.  A more complete example might fetch
 // from an existing data source like a REST API or database.
 const books = [
   {
-    title: "Harry Potter and the Chamber of Secrets",
-    author: "J.K. Rowling"
+    title: 'Harry Potter and the Chamber of Secrets',
+    author: 'J.K. Rowling',
   },
   {
-    title: "Jurassic Park",
-    author: "Michael Crichton"
-  }
-];
+    title: 'Jurassic Park',
+    author: 'Michael Crichton',
+  },
+]
 
 // Type definitions define the "shape" of your data and specify
 // which ways the data can be fetched from the GraphQL server.
@@ -30,23 +30,23 @@ const typeDefs = gql`
   type Query {
     books: [Book]
   }
-`;
+`
 
 // Resolvers define the technique for fetching the types in the
 // schema.  We'll retrieve books from the "books" array above.
 const resolvers = {
   Query: {
-    books: () => books
-  }
-};
+    books: () => books,
+  },
+}
 
 // In the most basic sense, the ApolloServer can be started
 // by passing type definitions (typeDefs) and the resolvers
 // responsible for fetching the data for those types.
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({typeDefs, resolvers})
 
 // This `listen` method launches a web-server.  Existing apps
 // can utilize middleware options, which we'll discuss later.
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
-});
+server.listen().then(({url}) => {
+  console.log(`🚀  Server ready at ${url}`) // eslint-disable-line
+})
