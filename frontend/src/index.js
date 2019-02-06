a03899b845bd8fa3ba7e137af1002421ef15bcda
Richard <richardizip@gmail.com>
2019-02-06 16:38:29 +0100

    Add flow to relevant files and fix errors

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,5 @@
+// @flow
+
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {ApolloClient} from 'apollo-client'
@@ -14,12 +16,15 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 })
 
-ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
-  document.getElementById('root')
-)
+const rootElement = document.getElementById('root')
+
+rootElement &&
+  ReactDOM.render(
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>,
+    rootElement
+  )
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
