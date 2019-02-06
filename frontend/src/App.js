cc396d9196960aaf1e83a0b2b96d975ea806635b
Patrik Prevužňák <patrik.prevuznak@gmail.com>
2019-02-06 10:29:08 +0100

    Rewrite graphql query with HOC

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
-import { Query } from 'react-apollo';
+import { graphql } from 'react-apollo';
 import { GET_TRANSACTION_BY_ID } from './api/queries';
 
 const txId = 'ef90f4873a27fdb64d09d58447e39f6b855cecd6303188f9648349fdc876592b'
 
-class App extends Component {
-  render() {
-    return (
-      <Query query={GET_TRANSACTION_BY_ID} variables={{ txId }}>
-        {({loading, error, data}) => 
-          <>
-            <h1>
-              Welcome to Seiza! 
-            </h1>
-            <h2>Cardano's next generation blockchain explorer.</h2>
-            <p>{!loading && JSON.stringify(data, null, 4)}</p>
-          </>
-        }
-      </Query>
-    );
-  }
+const withTransactionById = graphql(GET_TRANSACTION_BY_ID, {
+  name: 'transaction',
+  options: () => ({
+    variables: { txId },
+  }),
+});
+
+const App = (props) => {
+  const {loading, transaction} = props.transaction
+  return (
+    <>
+      <h1>
+        Welcome to Seiza! 
+      </h1>
+      <h2>Cardano's next generation blockchain explorer.</h2>
+      <p>{!loading && JSON.stringify(transaction, null, 4)}</p>
+    </>    
+  );
 }
 
-export default App;
+export default withTransactionById(App)
