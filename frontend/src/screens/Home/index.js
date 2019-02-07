10db02e5948e21ea492a3604c9e295445e4e8660
Peter Peresini <peter.peresini@gmail.com>
2019-02-07 14:46:50 +0100

    Introduce card displaying overview metrics

--- frontend/src/screens/Home/index.js
+++ frontend/src/screens/Home/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
+import MetricsCard from '../../components/visual/MetricsCard'
+
 const Blockchain = () => {
   return (
     <React.Fragment>
       <h1>Home</h1>
+      {/* temporary example*/}
+      <MetricsCard icon="blocks" value="-1" metric="Blocks" />
     </React.Fragment>
   )
 }
