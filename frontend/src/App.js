a3531724ab1693042b1dbee106737119a37782be
Peter Peresini <peter.peresini@gmail.com>
2019-02-08 12:37:43 +0100

    Basic styling of top bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import Staking from './screens/Staking'
 import More from './screens/More'
 
 import './App.css'
+import seizaLogo from './seiza-logo.png'
 
 // TODO: move elsewhere once there is folder structure
 const LanguageSwitch = withSetLocale(({setLocale}) => (
@@ -27,19 +28,35 @@ const LanguageSwitch = withSetLocale(({setLocale}) => (
   </React.Fragment>
 ))
 
+const styles = {
+  topbar: {
+    display: 'flex',
+    borderBottom: '1px solid gray',
+  },
+}
+const Spacer = () => <div style={{flexGrow: 1}} />
+
+const TopBar = () => (
+  <div style={styles.topbar}>
+    <img src={seizaLogo} />
+    <Spacer />
+    <Navbar
+      items={[
+        {link: routeTo.home(), label: 'Home'},
+        {link: routeTo.blockchain(), label: 'Blockchain'},
+        {link: routeTo.staking(), label: 'Staking'},
+        {link: routeTo.more(), label: 'More'},
+      ]}
+    />
+    <LanguageSwitch />
+  </div>
+)
+
 const App = () => {
   return (
     <Router>
       <React.Fragment>
-        <LanguageSwitch />
-        <Navbar
-          items={[
-            {link: routeTo.home(), label: 'Home'},
-            {link: routeTo.blockchain(), label: 'Blockchain'},
-            {link: routeTo.staking(), label: 'Staking'},
-            {link: routeTo.more(), label: 'More'},
-          ]}
-        />
+        <TopBar />
         <Route exact path={routeTo.home()} component={Home} />
         <Route path={routeTo.blockchain()} component={Blockchain} />
         <Route path={routeTo.staking()} component={Staking} />
