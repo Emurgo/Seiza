b88431244c725cb654bacd6d5c5ec5fb67138061
Nicolas Arqueros <nykholas@gmail.com>
2019-02-10 06:39:55 -0500

    back to no PORT env

--- graphql-server/src/index.js
+++ graphql-server/src/index.js
@@ -48,6 +48,6 @@ const server = new ApolloServer({
   }),
 })
 
-server.listen({port: process.env.PORT || 4000}).then(({url}) => {
+server.listen().then(({url}) => {
   console.log(`🚀  Server ready at ${url}`) // eslint-disable-line
 })
