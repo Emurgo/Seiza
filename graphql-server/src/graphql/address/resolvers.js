c9da1bb57ae1e82e8903e9709624ad77ad7918e5
Patrik Prevužňák <patrik.prevuznak@gmail.com>
2019-02-11 09:07:03 +0100

    Add types to Address resolver context

--- graphql-server/src/graphql/address/resolvers.js
+++ graphql-server/src/graphql/address/resolvers.js
@@ -1,10 +1,15 @@
+// @flow
 import {transactionResolver} from '../transaction/resolvers'
 import {facadeAddress} from './dataFacades'
 
+import type {ApolloContext} from '../../'
 // Note: for now this always fetche all transaction data, we
 // may consider optimization later if required
+type AddrArgs = {
+  id: string,
+}
 
-export const addressResolver = (parent, args, context) =>
+export const addressResolver = (parent, args: AddrArgs, context: ApolloContext) =>
   context.cardanoAPI.get(`addresses/summary/${args.id}`).then((data) => ({
     ...facadeAddress(data),
     transactions: data.caTxList.map((transaction) =>
