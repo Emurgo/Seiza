e551600d9670da9cb85d8e41dd368e4b3cb81f73
Peter Peresini <peter.peresini@gmail.com>
2019-02-14 09:10:17 +0100

    Move schema definitions to gql files

---
+++ graphql-server/src/gqlFlowShim.js
@@ -0,0 +1,6 @@
+/* @flow */
+// We need this file so that flow supports importing from .gql files
+// (see .flowconfig module mappers)
+import type {DocumentNode} from 'graphql'
+
+declare export default DocumentNode
