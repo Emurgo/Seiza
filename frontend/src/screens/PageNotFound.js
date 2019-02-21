e6bb7f4994522288590555803d8f68749911840f
Richard <richardizip@gmail.com>
2019-02-21 14:03:24 +0100

    Style page not found

--- frontend/src/screens/PageNotFound.js
+++ frontend/src/screens/PageNotFound.js
@@ -1,8 +1,39 @@
 import React from 'react'
-import {SimpleLayout} from '@/components/visual'
+import {Grid, Typography, withStyles, createStyles} from '@material-ui/core'
+import {defineMessages} from 'react-intl'
+import {compose} from 'redux'
 
-export default () => (
-  <SimpleLayout title="404">
-    <h4>We are sorry, but the requested url could not be found</h4>
-  </SimpleLayout>
+import {withI18n} from '@/i18n/helpers'
+
+const I18N_PREFIX = 'notFound'
+
+const messages = defineMessages({
+  notFound: {
+    id: `${I18N_PREFIX}.notFound`,
+    defaultMessage: 'We are sorry, but the requested url could not be found.',
+  },
+})
+
+const styles = createStyles({
+  wrapper: {
+    height: '100%',
+  },
+})
+
+const PageNotFound = ({classes, i18n: {translate}}) => (
+  <Grid
+    className={classes.wrapper}
+    container
+    justify="center"
+    alignItems="center"
+    direction="column"
+  >
+    <Typography variant="h1">404</Typography>
+    <Typography variant="h6">{translate(messages.notFound)}</Typography>
+  </Grid>
 )
+
+export default compose(
+  withI18n,
+  withStyles(styles)
+)(PageNotFound)
