9d36382c0dafa862139d8c13427c6f87f22d4168
Richard <richardizip@gmail.com>
2019-02-22 14:25:55 +0100

    Create KeyValueCard component

---
+++ frontend/src/components/visual/KeyValueCard.js
@@ -0,0 +1,100 @@
+// @flow
+import React from 'react'
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  Typography,
+  Grid,
+  createStyles,
+  withStyles,
+} from '@material-ui/core'
+
+const headerStyles = ({palette}) =>
+  createStyles({
+    wrapper: {
+      backgroundColor: palette.grey[200],
+      minHeight: '60px',
+      paddingLeft: '30px',
+      paddingRight: '30px',
+    },
+    label: {
+      color: palette.grey[500],
+    },
+  })
+
+const bodyStyles = ({palette}) =>
+  createStyles({
+    wrapper: {
+      paddingLeft: '15px',
+      paddingRight: '15px',
+    },
+    row: {
+      'paddingTop': '15px',
+      'paddingBottom': '15px',
+      'borderBottom': `1px solid ${palette.grey[200]}`,
+      '&:last-child': {
+        borderBottom: 'none',
+      },
+    },
+    label: {
+      color: palette.grey[500],
+    },
+  })
+
+const mainStyles = () =>
+  createStyles({
+    wrapper: {
+      width: '100%',
+    },
+  })
+
+const _KeyValueCard = ({classes, children, header}) => (
+  <Card className={classes.wrapper}>
+    <CardHeader component={() => header} />
+    <CardContent>{children}</CardContent>
+  </Card>
+)
+
+const KeyValueCard = withStyles(mainStyles)(_KeyValueCard)
+
+const _Header = ({classes, logo, label, value}) => (
+  <Grid container alignItems="center" className={classes.wrapper} justify="space-between">
+    {logo && (
+      <Grid item className={classes.logo}>
+        {logo}
+      </Grid>
+    )}
+    <Grid item>
+      <Typography className={classes.label}>{label}</Typography>
+    </Grid>
+    <Grid item className={classes.value}>
+      {value}
+    </Grid>
+  </Grid>
+)
+
+KeyValueCard.Header = withStyles(headerStyles)(_Header)
+
+const _Body = ({items, classes}) => (
+  <Grid container className={classes.wrapper}>
+    {items.map(({label, value}) => (
+      <Grid
+        container
+        key={label}
+        justify="space-between"
+        alignItems="center"
+        className={classes.row}
+      >
+        <Grid item>
+          <Typography className={classes.label}>{label}</Typography>
+        </Grid>
+        <Grid item>{value}</Grid>
+      </Grid>
+    ))}
+  </Grid>
+)
+
+KeyValueCard.Body = withStyles(bodyStyles)(_Body)
+
+export default KeyValueCard
--- frontend/src/screens/Home/index.js
+++ frontend/src/screens/Home/index.js
@@ -2,9 +2,11 @@
 import React from 'react'
 
 import BlockchainHeader from '@/screens/Blockchain/BlockchainHeader'
+import GeneralInfo from './GeneralInfo'
 
 export default () => (
   <React.Fragment>
     <BlockchainHeader />
+    <GeneralInfo />
   </React.Fragment>
 )
