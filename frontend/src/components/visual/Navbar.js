9a64e57c790c689e2160c5b04de819c6f568b4f4
Richard <richardizip@gmail.com>
2019-02-25 18:02:12 +0100

    Add dark theme and little of styling

--- frontend/src/components/visual/Navbar.js
+++ frontend/src/components/visual/Navbar.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import {Typography, withStyles} from '@material-ui/core'
+import {Typography, withStyles, createStyles} from '@material-ui/core'
 import classnames from 'classnames'
 
-const styles = ({palette}) => ({
-  list: {listStyleType: 'none'},
-  item: {display: 'inline', margin: '5px', marginRight: '40px'},
-  link: {textDecoration: 'none', textTransform: 'uppercase'},
-  linkText: {display: 'inline-block', color: palette.grey[700]},
-  active: {color: palette.primary.dark, borderBottom: `1px solid ${palette.primary.dark}`},
-})
+const styles = ({palette}) =>
+  createStyles({
+    list: {listStyleType: 'none'},
+    item: {display: 'inline', margin: '5px', marginRight: '40px'},
+    link: {textDecoration: 'none', textTransform: 'uppercase'},
+    linkText: {display: 'inline-block', color: palette.getContrastText(palette.background.default)},
+    active: {color: palette.primary.dark, borderBottom: `1px solid ${palette.primary.dark}`},
+  })
 
 // Note: as we do not except inner navigation, for simplicity we only check first level
 const getFirstLevelUrl = (pathname) =>
