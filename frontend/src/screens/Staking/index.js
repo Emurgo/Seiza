b2291503ef885f93ca2b65d142b9a7a9c80bf2de
Richard <richardizip@gmail.com>
2019-03-01 12:29:11 +0100

    Responsive staking screen

--- frontend/src/screens/Staking/index.js
+++ frontend/src/screens/Staking/index.js
@@ -11,22 +11,17 @@ import StakePoolList from './StakeList'
 import StakePoolHeader from './Header'
 import PageNotFound from '../PageNotFound'
 
-const SIDEBAR_WIDTH = 400
-
 const styles = () =>
   createStyles({
     sidebar: {
-      width: `${SIDEBAR_WIDTH}px`,
-    },
-    content: {
-      flex: 1,
-      paddingRight: `${SIDEBAR_WIDTH}px`,
+      maxWidth: '400px',
     },
     notFound: {
       marginTop: '100px',
     },
     wrapper: {
-      overflow: 'auto',
+      // TODO: consider a better solution to avoid scroll bars
+      overflow: 'hidden',
     },
   })
 
@@ -37,18 +32,25 @@ const NotFound = withStyles(styles)(({classes}) => (
 ))
 
 export default compose(withStyles(styles))(({classes}) => (
-  <Grid container direction="column">
-    <StakePoolHeader />
-    <Grid container direction="row" wrap="nowrap" className={classes.wrapper}>
-      <Grid item className={classes.sidebar}>
-        <SideMenu />
-      </Grid>
-      <Grid item className={classes.content}>
-        <Switch>
-          <Redirect exact from={routeTo.staking.home()} to={routeTo.staking.poolList()} />
-          <Route exact path={routeTo.staking.poolList()} component={StakePoolList} />
-          <Route component={NotFound} />
-        </Switch>
+  <Grid container direction="column" className={classes.wrapper}>
+    <Grid item>
+      <StakePoolHeader />
+    </Grid>
+    <Grid item>
+      <Grid container direction="row" wrap="nowrap" spacing={24}>
+        <Grid item lg={3} xl={3}>
+          <div className={classes.sidebar}>
+            <SideMenu />
+          </div>
+        </Grid>
+        <Grid item lg={9} xl={6} className={classes.mainContent}>
+          <Switch>
+            <Redirect exact from={routeTo.staking.home()} to={routeTo.staking.poolList()} />
+            <Route exact path={routeTo.staking.poolList()} component={StakePoolList} />
+            <Route component={NotFound} />
+          </Switch>
+        </Grid>
+        <Grid item lg={0} xl={3} />
       </Grid>
     </Grid>
   </Grid>
