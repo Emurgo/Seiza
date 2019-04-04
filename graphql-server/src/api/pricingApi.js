a8ddee37c3b3d82f8af4f7858a58d8a950e69c2a
Richard <richardizip@gmail.com>
2019-04-04 11:45:46 +0200

    Add tests and move to utils

--- graphql-server/src/api/pricingApi.js
+++ graphql-server/src/api/pricingApi.js
@@ -1,25 +1,10 @@
 // @flow
-import axios from 'axios'
-import {setupCache} from 'axios-cache-adapter'
-
-import {objStripNullValues} from './utils'
+import {objStripNullValues, getCachedApi} from './utils'
 
 const API_ROOT = 'https://min-api.cryptocompare.com/data/'
 
-const cache = setupCache({
-  maxAge: 20 * 1000,
-  exclude: {
-    query: false,
-  },
-  // https://github.com/RasCarlito/axios-cache-adapter/issues/31
-  key: (req) => req.url + JSON.stringify(req.params),
-  // Not: sure how to set this number, but we should probably not allow infinite space
-  limit: 1000,
-})
-
-const api = axios.create({
-  adapter: cache.adapter,
-})
+// Not sure how to set `limit`, but we should probably not allow infinite space
+const api = getCachedApi({maxAge: 20 * 1000, limit: 1000})
 
 export const pricingAPI = {
   get: (path: string, query: any) => {
---
+++ graphql-server/src/api/utils.test.js
@@ -0,0 +1,80 @@
+import express from 'express'
+import cors from 'cors'
+
+import {getCachedApi} from './utils'
+
+const sleep = (milliseconds) => {
+  return new Promise((resolve) => setTimeout(resolve, milliseconds))
+}
+
+const getDummyServer = (port) => {
+  const server = express()
+  server.use(cors())
+
+  server.get('/test1', (req, res) => {
+    res.send('OK1')
+  })
+
+  server.get('/test2', (req, res) => {
+    res.send('OK2')
+  })
+
+  return server.listen(port)
+}
+
+describe('Cache tests', () => {
+  const PORT = 3008
+  const DOMAIN = 'http://localhost'
+  let server = null
+
+  beforeEach(() => {
+    server = getDummyServer(PORT)
+  })
+
+  afterEach(() => {
+    server.close()
+  })
+
+  test('Cache second request', async () => {
+    const api = getCachedApi({maxAge: 1000 * 10, limit: 10})
+
+    const res1 = await api.get(`${DOMAIN}:${PORT}/test1`)
+    expect(res1.request.fromCache).not.toBe(true)
+
+    const res2 = await api.get(`${DOMAIN}:${PORT}/test1`)
+    expect(res2.request.fromCache).toBe(true)
+  })
+
+  test('Dont cache after max age', async () => {
+    const api = getCachedApi({maxAge: 1000 * 1, limit: 10})
+
+    const res1 = await api.get(`${DOMAIN}:${PORT}/test1`)
+    expect(res1.request.fromCache).not.toBe(true)
+
+    await sleep(1500)
+
+    const res2 = await api.get(`${DOMAIN}:${PORT}/test1`)
+    expect(res2.request.fromCache).not.toBe(true)
+  })
+
+  test('Two endpoints cache', async () => {
+    const api = getCachedApi({maxAge: 1000 * 10, limit: 10})
+
+    const endpoint1Res1 = await api.get(`${DOMAIN}:${PORT}/test1`)
+    expect(endpoint1Res1.request.fromCache).not.toBe(true)
+
+    const endpoint2Res1 = await api.get(`${DOMAIN}:${PORT}/test2`)
+    expect(endpoint2Res1.request.fromCache).not.toBe(true)
+
+    const endpoint1Res2 = await api.get(`${DOMAIN}:${PORT}/test1`)
+    expect(endpoint1Res2.request.fromCache).toBe(true)
+    expect(endpoint1Res2.data).toBe('OK1')
+
+    const endpoint2Res2 = await api.get(`${DOMAIN}:${PORT}/test2`)
+    expect(endpoint2Res2.request.fromCache).toBe(true)
+    expect(endpoint2Res2.data).toBe('OK2')
+  })
+
+  // TODO: tests for query params requests
+  // TODO: tests for limit
+})
