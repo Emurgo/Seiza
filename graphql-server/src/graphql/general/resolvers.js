0e0206bdea07ef36022b87daabcfd7a5773d2bd7
Richard <richardizip@gmail.com>
2019-04-11 08:46:19 +0200

    GroupBy day/epoch, add totalAdaSent and total utxo

--- graphql-server/src/graphql/general/resolvers.js
+++ graphql-server/src/graphql/general/resolvers.js
@@ -1,4 +1,5 @@
 // @flow
+import _ from 'lodash'
 import {fetchGeneralInfo, fetchSlotInfo, fetchEpochInfo} from './dataProviders'
 import {parseAdaValue} from '../utils'
 import assert from 'assert'
@@ -16,75 +17,128 @@ const fetchSupplyAfterSlot = async ({elastic, E}, {epoch, slot}) => {
   return parseAdaValue(hit._source.supply_after_this_tx)
 }
 
-const fetchTxCountGraph = async ({elastic, E}) => {
-  const aggByEpoch = (defs) => ({
-    encode: () => ({
-      terms: {
-        field: 'epoch',
-        size: 10000,
-        show_term_doc_count_error: true,
+const aggByEpoch = (E, {from, to}, defs) => ({
+  encode: () => ({
+    filter: E.filter([from && E.gte('epoch', from), to && E.lte('epoch', to)]),
+    aggs: {
+      __filtered: {
+        terms: {
+          field: 'epoch',
+          size: 10000,
+          show_term_doc_count_error: true,
+          order: {_key: 'asc'},
+        },
+        aggs: E.agg._encode(defs),
       },
-      aggs: E.agg._encode(defs),
+    },
+  }),
+  decode: (x) =>
+    x.__filtered.buckets.map((bucket) => {
+      assert(bucket.doc_count_error_upper_bound === 0)
+      return {
+        key: bucket.key,
+        ...E.agg._decode(defs, bucket),
+      }
     }),
-    decode: (x) =>
-      x.buckets.map((bucket) => {
-        assert(bucket.doc_count_error_upper_bound === 0)
-        return {
-          key: bucket.key,
-          ...E.agg._decode(defs, bucket),
-        }
-      }),
-  })
+})
 
-  const filterRecent = (since, defs) => ({
-    encode: () => ({
-      filter: E.gte('time', since),
-      aggs: E.agg._encode(defs),
-    }),
-    decode: (x) => E.agg._decode(defs, x),
-  })
-
-  const aggByDay = (since, defs) => ({
-    encode: () => ({
-      filter: E.gte('time', since),
+const aggByDay = (E, {from, to}, defs) => ({
+  encode: () => {
+    const dayInMs = 1000 * 60 * 60 * 24
+    from && assert(from.valueOf() % dayInMs === 0)
+    to && assert(to.valueOf() % dayInMs === 0)
+    return {
+      filter: E.filter([
+        from && E.gte('time', from.valueOf()),
+        to && E.lt('time', to.valueOf() + dayInMs),
+      ]),
       aggs: {
         __filtered: {
           date_histogram: {
             field: 'time',
             format: 'yyyy-MM-dd',
             interval: '1d',
-            min_doc_count: 0,
-            extended_bounds: {
-              max: 'now',
-              min: since,
-            },
+            // Note: not using `extended_bounds` intentionally,
+            // we show data only from time we have them
           },
           aggs: E.agg._encode(defs),
         },
       },
+    }
+  },
+  decode: (x) =>
+    x.__filtered.buckets.map((bucket) => {
+      return {
+        key: bucket.key_as_string,
+        ...E.agg._decode(defs, bucket),
+      }
     }),
-    decode: (x) =>
-      x.__filtered.buckets.map((bucket) => {
-        return {
-          key: bucket.key_as_string,
-          ...E.agg._decode(defs, bucket),
-        }
-      }),
-  })
+})
 
-  const since = 'now-549d/d'
+const fetchAggregateInfo = ({elastic, E}, groupBy, epochInterval = {}, dateInterval = {}) => {
+  const wrapInGroupBy = (defs) => {
+    return {
+      DAY: aggByDay(E, dateInterval, defs),
+      EPOCH: aggByEpoch(E, epochInterval, defs),
+    }[groupBy]
+  }
 
-  const aggs = await elastic.q('slot').getAggregations({
-    byDay: aggByDay(since, {
-      txCount: E.agg.sum('tx_num'),
-    }),
+  const prepareSeries = (res, transform) => ({
+    data: res.map(({key, rawY}) => ({
+      x: key,
+      y: transform(rawY),
+    })),
   })
 
   return {
-    data: aggs.byDay.map(({key, txCount}) => ({
-      x: key,
-      y: txCount,
-    })),
+    totalAdaTransferred: () =>
+      elastic
+        .q('slot')
+        .getAggregations({
+          data: wrapInGroupBy({
+            rawY: E.agg.sumAda('sent'),
+          }),
+        })
+        .then(({data}) => prepareSeries(data, (y) => parseAdaValue(y).toNumber())),
+
+    txCount: () =>
+      elastic
+        .q('slot')
+        .getAggregations({
+          data: wrapInGroupBy({
+            rawY: E.agg.sum('tx_num'),
+          }),
+        })
+        .then(({data}) => prepareSeries(data, (y) => y)),
+
+    totalUtxoCreated: async () => {
+      const res1 = await elastic.q('tx').getAggregations({
+        data: wrapInGroupBy({
+          rawY: E.agg.count('outputs.id.keyword'),
+        }),
+      })
+
+      const res2 = await elastic
+        .q('txio')
+        .filter(E.matchPhrase('type', 'output'))
+        .getAggregations({
+          data: wrapInGroupBy({
+            rawY: E.agg.count('id.keyword'),
+          }),
+        })
+
+      const res3 = await elastic.q('slot').getAggregations({
+        data: wrapInGroupBy({
+          rawY: E.agg.count('tx.outputs.id.keyword'),
+        }),
+      })
+
+      // Note: we could use only `res1`, `res2` or `res3`, this is only paranoid check
+      assert(_.isEqual(res1, res2))
+      assert(_.isEqual(res2, res3))
+
+      return prepareSeries(res1.data, (y) => y)
+    },
   }
 }
 
@@ -93,7 +147,8 @@ export default {
     generalInfo: (root: any, args: any, context: any) => fetchGeneralInfo(context, args.infoPeriod),
     epochInfo: (root: any, args: any, context: any) => fetchEpochInfo(context, args.epoch),
     slotInfo: (root: any, args: any, context: any) => fetchSlotInfo(context, args.epoch, args.slot),
-    txCountGraph: (root: any, args: any, context: any) => fetchTxCountGraph(context),
+    aggregateInfo: (root: any, args: any, context: any) =>
+      fetchAggregateInfo(context, args.groupBy, args.epochInterval, args.dateInterval),
   },
   SlotInfo: {
     supply: (slotInfo: any, args: any, context: any) =>
