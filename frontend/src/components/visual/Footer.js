2414f6fde68fea3bf8ac96428174f32e4ecf1965
Patrik Prevužňák <patrik.prevuznak@gmail.com>
2019-04-30 17:16:37 +0200

    Add UI interactivity to subcribe footer

--- frontend/src/components/visual/Footer.js
+++ frontend/src/components/visual/Footer.js
@@ -6,15 +6,27 @@ import {defineMessages} from 'react-intl'
 import gql from 'graphql-tag'
 import {useMutation} from 'react-apollo-hooks'
 import IsEmail from 'isemail'
-import {Grid, Typography, TextField} from '@material-ui/core'
+import idx from 'idx'
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
+import {
+  Grid,
+  Typography,
+  FormControl,
+  FormHelperText,
+  // $FlowFixMe: Weird error: Cannot import OutlinedInput from @material-ui/core.
+  OutlinedInput,
+} from '@material-ui/core'
 import {makeStyles} from '@material-ui/styles'
 import {darken} from '@material-ui/core/styles/colorManipulator'
 
 import {useI18n} from '@/i18n/helpers'
 import {routeTo} from '@/helpers/routes'
 import analytics from '@/helpers/googleAnalytics'
-import {Button, ExternalLink, Tooltip} from '@/components/visual'
-import logo from '../../assets/icons/logo-seiza-white.svg'
+import {Button, ExternalLink, Tooltip, CloseIconButton, LoadingOverlay} from '@/components/visual'
+import logo from '@/assets/icons/logo-seiza-white.svg'
+import alertIcon from '@/assets/icons/alert.svg'
+import subscribedIcon from '@/assets/icons/subscribed.svg'
+import useLocalStorage from '@/components/hooks/useLocalStorage'
 
 import fbIcon from '@/assets/icons/social/fb.svg'
 import linkedInIcon from '@/assets/icons/social/linkedin.svg'
@@ -35,6 +47,9 @@ const subscribeMessages = defineMessages({
     'Subscribe to Seiza to receive news and updates about staking, rewards and new features 🚀!',
   emailButton: 'Email',
   subscribeButton: 'Subscribe',
+  genericError: 'Sorry. An error occurred. Please try again.',
+  invalidEmail: 'Incorrect format of Email',
+  subscribeSuccess: 'You are successfully subscribed!',
 })
 
 const SOCIAL_LINKS = {
@@ -48,35 +63,59 @@ const SOCIAL_LINKS = {
 
 const useRoundedInputStyles = makeStyles((theme) => {
   return {
-    // Mui Input div
-    Input: {
-      '&>fieldset': {
-        borderRadius: '35px',
-        borderColor: '#9881DC !important',
-      },
-      'height': '100%',
-    },
-    // <input> element
     input: {
       padding: '8.5px 15px',
+    },
+    errorLabel: {
+      position: 'absolute',
+      top: 0,
+      right: 0,
+      width: '90%',
+    },
+    formControl: {
+      height: '100%',
+    },
+    errorIcon: {
+      height: '70%',
+    },
+    relative: {
+      position: 'relative',
     },
   }
 })
 
-const RoundedInput = React.forwardRef((props, ref) => {
+const useOutlinedInputStyles = makeStyles(() => ({
+  root: {
+    height: '100%',
+  },
+  notchedOutline: {
+    borderRadius: '35px',
+    // TODO: get from theme
+    borderColor: '#9881DC !important',
+  },
+}))
+
+const RoundedInput = ({errorMessage, ...props}) => {
   const classes = useRoundedInputStyles()
+  const outlinedInputClasses = useOutlinedInputStyles()
   return (
-    <TextField
-      variant="outlined"
-      InputProps={{className: classes.Input}}
-      // (react-create-app complains about same props)
-      // eslint-disable-next-line
-      inputProps={{className: classes.input}}
-      inputRef={ref}
-      {...props}
-    />
+    <FormControl className={classes.formControl}>
+      <OutlinedInput
+        inputProps={{className: classes.input}}
+        classes={outlinedInputClasses}
+        endAdornment={
+          errorMessage ? <img alt="" src={alertIcon} className={classes.errorIcon} /> : null
+        }
+        {...props}
+      />
+      <div className={classes.relative}>
+        <FormHelperText error className={classes.errorLabel}>
+          {errorMessage}
+        </FormHelperText>
+      </div>
+    </FormControl>
   )
-})
+}
 
 const SUBSCRIBE_MUTATION = gql`
   mutation subscribeToNewsletter($email: String!) {
@@ -85,91 +124,279 @@ const SUBSCRIBE_MUTATION = gql`
 `
 const useSubscribeMutation = (email) => useMutation(SUBSCRIBE_MUTATION, {variables: {email}})
 
-const useSubscribeFooterStyles = makeStyles(({palette, spacing}) => ({
-  wrapper: {
+const LARGEST_FOOTER_HEIGHT = 260
+
+const useSubscribeFooterStyles = makeStyles(({palette, spacing, breakpoints}) => ({
+  '@global': {
+    '@keyframes footer-leave': {
+      '0%': {
+        opacity: 1,
+        maxHeight: LARGEST_FOOTER_HEIGHT,
+      },
+      '50%': {
+        opacity: 0,
+        maxHeight: LARGEST_FOOTER_HEIGHT,
+      },
+      '100%': {
+        maxHeight: 0,
+        padding: 0,
+        opacity: 0,
+      },
+    },
+  },
+  'wrapper': {
     padding: spacing.unit * 2,
     background: palette.gradient,
+    position: 'relative',
+    height: 200,
+    [breakpoints.down('xs')]: {
+      height: LARGEST_FOOTER_HEIGHT,
+    },
   },
-  subscribe: {
+  'subscribe': {
     marginLeft: spacing.unit * 1.3,
     marginRight: spacing.unit * 1.3,
     width: '200px',
   },
-  email: {
+  'email': {
     width: '280px',
     height: '100%',
   },
-  row: {
+  'row': {
     padding: spacing.unit,
   },
-  subscribeHeadlineWrapper: {
+  'subscribeHeadlineWrapper': {
     padding: spacing.unit * 1.5,
+  },
+  'success': {
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+  },
+  'successWrapper': {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+  },
+  'hide': {
+    position: 'absolute',
+    top: 12,
+    right: 12,
+  },
+  'initUILeave': {
+    opacity: 1,
+    transform: 'translateY(0px)',
+  },
+  'initUILeaveActive': {
+    opacity: 0,
+    transform: 'translateY(-100px)',
+    transition: 'all 1s ease-in-out',
+  },
+  'successUIEnter': {
+    opacity: 0,
+    transform: 'translateX(100px)',
+  },
+  'successUIEnterActive': {
+    opacity: 1,
+    transition: 'all 1s ease-in-out 500ms',
+    transform: 'translateX(0px)',
+  },
+  'hideLeave': {}, // must be defined anyway
+  'hideLeaveActive': {
+    animation: 'footer-leave 2000ms',
   },
 }))
 
-// TODO: Save subscribed boolean to local storage and hide this section
+type UiState = 'success' | 'error' | 'loading' | 'init'
+
+const useUIState = (initialUIState: UiState) => {
+  const [uiState, setUIState] = useState<UiState>('init')
+  const [errorMessage, setErrorMessage] = useState(null)
+  const setError = useCallback(
+    (error) => {
+      setUIState('error')
+      setErrorMessage(error)
+    },
+    [setUIState]
+  )
+  const setLoading = useCallback(() => setUIState('loading'), [setUIState])
+  const setSuccess = useCallback(() => setUIState('success'), [setUIState])
+  const setInit = useCallback(() => setUIState('init'), [setUIState])
+
+  return {
+    uiState,
+    setError,
+    setLoading,
+    setSuccess,
+    setInit,
+    errorMessage,
+  }
+}
+
+const checkInvalidEmailError = (error) =>
+  idx(error, (_) => _.graphQLErrors[0].extensions.code === 'INVALID_EMAIL')
+
 const SubscribeFooter = () => {
   const classes = useSubscribeFooterStyles()
   const {translate: tr} = useI18n()
   const [email, setEmail] = useState('')
+  const [hidden, setHidden] = useLocalStorage('subscribedToNewsletter', false)
+  const {uiState, setError, setSuccess, setLoading, errorMessage} = useUIState('init')
+
   const subscribe = useSubscribeMutation(email)
+
   const onEmailChange = useCallback((event) => setEmail(event.target.value), [setEmail])
+
+  const setInvalidEmailError = useCallback(
+    (event) => {
+      setError(tr(subscribeMessages.invalidEmail))
+    },
+    [setError, tr]
+  )
+
+  const onHide = useCallback(() => {
+    setHidden(true)
+  }, [setHidden])
+
   const validateAndSubscribe = useCallback(
     (event) => {
       event.preventDefault()
-      // TODO: some visual feedback for user in case of invalid email?
-      if (IsEmail.validate(email)) {
+      if (!IsEmail.validate(email)) {
+        setInvalidEmailError()
+      } else {
+        setLoading()
         subscribe(email)
-        setEmail('')
+          .then((res) => {
+            if (res.data.subscribeToNewsletter) {
+              setSuccess()
+              setEmail('')
+              setTimeout(onHide, 5000)
+            } else {
+              throw new Error('SubscribeError')
+            }
+          })
+          .catch((error) => {
+            const isEmailInvalidError = checkInvalidEmailError(error)
+            isEmailInvalidError
+              ? setInvalidEmailError()
+              : setError(tr(subscribeMessages.genericError))
+          })
       }
-      // TODO: Add some visual feedback for the user that he successfully subscribed / error ocurred
     },
-    [email, subscribe]
+    [email, setInvalidEmailError, setLoading, subscribe, setSuccess, onHide, setError, tr]
   )
 
   return (
-    <Grid
-      className={classes.wrapper}
-      container
-      direction="column"
-      justify="center"
-      alignItems="center"
+    <ReactCSSTransitionGroup
+      transitionName={{
+        leave: classes.hideLeave,
+        leaveActive: classes.hideLeaveActive,
+      }}
+      transitionLeave
+      transitionEnter={false}
+      transitionAppear={false}
+      component="div"
     >
-      <Grid item className={classes.row}>
-        <Grid container direction="column" alignItems="center">
-          <div className={classes.subscribeHeadlineWrapper}>
-            <Typography variant="h1">{tr(subscribeMessages.subscribeHeader)}</Typography>
-          </div>
-          <Typography variant="body1">{tr(subscribeMessages.subscribeText)}</Typography>
-        </Grid>
-      </Grid>
-
-      <Grid item className={classes.row}>
-        <form>
-          <Grid container direction="row" justify="center" spacing={16}>
-            <Grid item>
-              <RoundedInput
-                className={classes.email}
-                placeholder={tr(subscribeMessages.emailButton)}
-                type="email"
-                onChange={onEmailChange}
-              />
-            </Grid>
-            <Grid item>
-              <Button
-                rounded
-                gradient
-                className={cn(classes.subscribe)}
-                type="submit"
-                onClick={validateAndSubscribe}
+      {!hidden && (
+        <Grid
+          className={classes.wrapper}
+          container
+          direction="column"
+          justify="center"
+          alignItems="center"
+        >
+          <ReactCSSTransitionGroup
+            transitionName={{
+              enter: classes.successUIEnter,
+              enterActive: classes.successUIEnterActive,
+              appear: classes.successUIEnter,
+              appearActive: classes.successUIEnterActive,
+            }}
+            transitionLeave={false}
+            transitionEnter
+            transitionAppear
+            component={React.Fragment}
+          >
+            {uiState === 'success' && (
+              <Grid
+                container
+                direction="column"
+                justify="center"
+                alignItems="center"
+                className={classes.successWrapper}
               >
-                {tr(subscribeMessages.subscribeButton)}
-              </Button>
-            </Grid>
-          </Grid>
-        </form>
-      </Grid>
-    </Grid>
+                <Grid item>
+                  <Typography className={classes.success} variant="body1">
+                    {tr(subscribeMessages.subscribeSuccess)}
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <img alt="" src={subscribedIcon} />
+                </Grid>
+              </Grid>
+            )}
+          </ReactCSSTransitionGroup>
+          <ReactCSSTransitionGroup
+            transitionName={{
+              leave: classes.initUILeave,
+              leaveActive: classes.initUILeaveActive,
+            }}
+            transitionLeave
+            transitionEnter={false}
+            transitionAppear={false}
+          >
+            {['error', 'loading', 'init'].includes(uiState) && (
+              <div>
+                <Grid item className={classes.row}>
+                  <Grid container direction="column" alignItems="center">
+                    <div className={classes.subscribeHeadlineWrapper}>
+                      <Typography variant="h1">{tr(subscribeMessages.subscribeHeader)}</Typography>
+                    </div>
+                    <Typography variant="body1">{tr(subscribeMessages.subscribeText)}</Typography>
+                  </Grid>
+                </Grid>
+
+                <Grid item className={classes.row}>
+                  <form>
+                    <Grid container direction="row" justify="center" spacing={16}>
+                      <Grid item>
+                        <RoundedInput
+                          className={classes.email}
+                          placeholder={tr(subscribeMessages.emailButton)}
+                          type="email"
+                          onChange={onEmailChange}
+                          errorMessage={errorMessage}
+                          aria-describedby="error-text"
+                        />
+                      </Grid>
+                      <Grid item>
+                        <Button
+                          rounded
+                          gradient
+                          className={cn(classes.subscribe)}
+                          type="submit"
+                          onClick={validateAndSubscribe}
+                        >
+                          {tr(subscribeMessages.subscribeButton)}
+                        </Button>
+                      </Grid>
+                    </Grid>
+                  </form>
+                </Grid>
+              </div>
+            )}
+          </ReactCSSTransitionGroup>
+
+          {uiState !== 'loading' && (
+            <CloseIconButton
+              aria-label="Hide-subscribe-footer"
+              className={classes.hide}
+              onClick={onHide}
+            />
+          )}
+          <LoadingOverlay loading={uiState === 'loading'} />
+        </Grid>
+      )}
+    </ReactCSSTransitionGroup>
   )
 }
 
@@ -316,7 +543,7 @@ const MainFooter = ({navItems}) => {
                       icon={linkedInIcon}
                       iconName="linkedin"
                     />
-                  </Grid>
+                  </Grid>{' '}
                 </Grid>
               </Grid>
             </Grid>
