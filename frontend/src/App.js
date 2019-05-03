419b40f7208c227c75765e960b61a3d3ce16ebc6
Richard <richardizip@gmail.com>
2019-05-03 16:34:23 +0200

    Align navigation demo 2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -51,7 +51,7 @@ const useAppStyles = makeStyles((theme) => ({
     flex: 1,
   },
   topBar: {
-    padding: `${theme.spacing.unit}px ${theme.spacing.unit * 0}px`,
+    padding: `${theme.spacing.unit}px ${theme.spacing.unit * 5}px`,
   },
 }))
 
