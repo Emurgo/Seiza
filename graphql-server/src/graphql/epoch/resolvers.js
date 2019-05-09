fa449fe269b15b2dbba58502d465a83e0a9a8899
Peter Peresini <peter.peresini@gmail.com>
2019-05-09 13:13:22 +0200

    Fix progress calculation for epoch

--- graphql-server/src/graphql/epoch/resolvers.js
+++ graphql-server/src/graphql/epoch/resolvers.js
@@ -1,3 +1,5 @@
+import assert from 'assert'
+
 import {
   fetchEpoch,
   fetchBlockCount,
@@ -5,10 +7,31 @@ import {
   fetchTotalAdaSupply,
   fetchTotalFees,
 } from './dataProviders'
+import {fetchCurrentSyncTime} from '../status/resolvers'
+
+const clamp = (min, max, value) => {
+  if (value < min) return min
+  if (value > max) return max
+  return value
+}
 
 export default {
   Query: {
     epoch: (root, args, context) => fetchEpoch(null, args.epochNumber),
+  },
+  Epoch: {
+    progress: async (epoch, args, context) => {
+      const {startTime, endTime} = epoch
+      const currentTime = await fetchCurrentSyncTime(context)
+
+      const startTs = startTime.unix()
+      const endTs = endTime.unix()
+      const currentTs = currentTime.unix()
+
+      assert(endTs > startTs)
+
+      return clamp(0, 1, (currentTs - startTs) / (endTs - startTs))
+    },
   },
   EpochSummary: {
     blocksCreated: async (summary, args, context) => {
