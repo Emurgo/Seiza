895427de5e6ca7b62ce2ce0f93592a0b433614f7
Roberto C. Morano <rcmova@gmail.com>
2019-05-10 14:17:16 +0200

    Fixed typo

--- frontend/heroku.js
+++ frontend/heroku.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 4000;
 
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('*', (req, res) => {
-  res.sendFile(__dirname + '/build/index.html'));
+  res.sendFile(__dirname + '/build/index.html');
 })
 
 //start server
