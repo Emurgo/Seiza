cfb7fd5ab8fb03b52bec94a75cc10e0bf287380d
Peter Peresini <peter.peresini@gmail.com>
2019-05-16 16:35:20 +0200

    Fix address sent & received totals

--- graphql-server/src/graphql/address/dataProviders.js
+++ graphql-server/src/graphql/address/dataProviders.js
@@ -1,27 +1,6 @@
 // @flow
 import assert from 'assert'
-import {parseAdaValue, annotateNotFoundError} from '../utils'
-import BigNumber from 'bignumber.js'
-import _ from 'lodash'
-
-const adaSum = (array) => array.reduce((x, y) => x.plus(y), new BigNumber(0))
-
-export const facadeAddress = (source: any, __id: string) => ({
-  address58: __id,
-  type: 'TODO: FIX IN ELASTIC',
-  transactionsCount: source.tx_num,
-  balance: parseAdaValue(source.balance),
-  totalAdaReceived: adaSum(
-    source.ios.filter((io) => io.type === 'output').map((io) => parseAdaValue(io.value))
-  ),
-  totalAdaSent: adaSum(
-    source.ios.filter((io) => io.type === 'input').map((io) => parseAdaValue(io.value))
-  ),
-  _transactionIds: _(source.ios.map((io) => io.tx_hash))
-    .uniq()
-    .reverse() // Note: we want transactions to be sorted newest-to-oldest
-    .value(),
-})
+import {parseAdaValue, annotateNotFoundError, validate, runConsistencyCheck} from '../utils'
 
 export const fetchAddress = async ({elastic, E}: any, address58: string) => {
   assert(address58)
@@ -31,5 +10,37 @@ export const fetchAddress = async ({elastic, E}: any, address58: string) => {
     .filter(E.matchPhrase('_id', address58))
     .getSingleHit()
     .catch(annotateNotFoundError({entity: 'Address'}))
-  return facadeAddress(hit._source, hit._id)
+
+  const ioTotalValue = (address58, type) =>
+    elastic
+      .q('txio')
+      .filter(E.matchPhrase('address', address58))
+      .filter(E.matchPhrase('type', type))
+      .getAggregations({
+        sum: E.agg.sumAda('value'),
+      })
+      .then(({sum}) => parseAdaValue(sum))
+
+  const totalAdaSent = ioTotalValue(address58, 'output')
+  const totalAdaReceived = ioTotalValue(address58, 'input')
+
+  const balance = parseAdaValue(hit._source.balance)
+
+  await runConsistencyCheck(async () => {
+    const [s, r] = await Promise.all([totalAdaSent, totalAdaReceived])
+
+    validate(s.minus(r).eq(balance), 'Inconsistency in address balance', {
+      txios_sent: s,
+      txios_received: r,
+      address_balance: balance,
+    })
+  })
+
+  return {
+    address58,
+    transactionsCount: hit._source.tx_num,
+    balance,
+    totalAdaSent,
+    totalAdaReceived,
+  }
 }
