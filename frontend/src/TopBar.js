1e6ad5286b4a6561cd2690c7731564356ede14ef
Peter Peresini <peter.peresini@gmail.com>
2019-06-10 09:39:59 +0200

    Extract TopBar to its own file

---
+++ frontend/src/TopBar.js
@@ -0,0 +1,77 @@
+// @flow
+import React from 'react'
+import {withRouter} from 'react-router'
+import {compose} from 'redux'
+import {Grid, Hidden} from '@material-ui/core'
+import {makeStyles} from '@material-ui/styles'
+import {fade} from '@material-ui/core/styles/colorManipulator'
+import cn from 'classnames'
+import {routeTo} from './helpers/routes'
+
+import Search from './screens/Blockchain/BlockchainHeader/Search'
+import seizaLogo from './assets/icons/logo-seiza.svg'
+import {Navbar, MobileNavbar, Link} from './components/visual'
+
+import LanguageSelect from '@/components/common/LanguageSelect'
+import ThemeSelect from '@/components/common/ThemeSelect'
+
+import config from './config'
+
+const useAppStyles = makeStyles((theme) => ({
+  topBar: {
+    position: 'relative',
+    background: theme.palette.background.paper,
+    boxShadow: `0px 5px 25px ${fade(theme.palette.shadowBase, 0.12)}`,
+    padding: theme.spacing.unit,
+    [theme.breakpoints.up('sm')]: {
+      padding: `${theme.spacing.unit}px ${theme.spacing.unit * 5}px`,
+    },
+  },
+  mobileSearch: {
+    flex: 1,
+    marginLeft: 0,
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing.unit * 2,
+    },
+  },
+}))
+
+const TopBar = compose(withRouter)(({location: {pathname}, navItems}) => {
+  const classes = useAppStyles()
+  return (
+    <React.Fragment>
+      <Hidden smDown>
+        <Grid
+          container
+          direction="row"
+          justify="space-between"
+          alignItems="center"
+          className={classes.topBar}
+        >
+          <Grid item>
+            <Link to={routeTo.home()}>
+              <img alt="" src={seizaLogo} />
+            </Link>
+          </Grid>
+          <Grid item>
+            <Grid container direction="row" alignItems="center">
+              <Navbar currentPathname={pathname} items={navItems} />
+              <LanguageSelect />
+              {config.featureEnableThemes && <ThemeSelect />}
+            </Grid>
+          </Grid>
+        </Grid>
+      </Hidden>
+      <Hidden mdUp>
+        <div className={cn(classes.topBar, 'd-flex')}>
+          <MobileNavbar currentPathname={pathname} items={navItems} />
+          <div className={classes.mobileSearch}>
+            <Search isMobile />
+          </div>
+        </div>
+      </Hidden>
+    </React.Fragment>
+  )
+})
+
+export default TopBar
