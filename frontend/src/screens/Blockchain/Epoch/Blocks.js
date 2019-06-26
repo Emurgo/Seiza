9bd796a48d5293b68d5de774daf2e4caac4761d6
Peter Peresini <peter.peresini@gmail.com>
2019-06-26 15:48:49 +0200

    Rework buggy pagination logic.

--- frontend/src/screens/Blockchain/Epoch/Blocks.js
+++ frontend/src/screens/Blockchain/Epoch/Blocks.js
@@ -2,6 +2,8 @@ import React, {useState, useEffect} from 'react'
 import {defineMessages} from 'react-intl'
 import {Grid, Hidden} from '@material-ui/core'
 import {makeStyles} from '@material-ui/styles'
+import _ from 'lodash'
+import assert from 'assert'
 
 import Pagination from '@/components/visual/Pagination'
 import {GET_PAGED_BLOCKS_IN_EPOCH} from '@/api/queries'
@@ -49,7 +51,7 @@ const useLoadData = (cursor, epochNumber) => {
   return {
     loading,
     error,
-    data: data.pagedBlocksInEpoch,
+    data: data.pagedBlocksInEpoch || {},
   }
 }
 
@@ -59,43 +61,78 @@ const columns = [SLOT, TIME, SLOT_LEADER, TRANSACTIONS, TOTAL_SENT, FEES, SIZE]
 const PAGE_SIZE = 10
 const rowsPerPage = PAGE_SIZE
 
+// Note(ppershing): This handler is quite complicated bacause of the way
+// we want to smoothly deal with pagination when both switching pages
+// and switching epochs
 const useEpochBlockData = (epochNumber) => {
-  const [prevEpoch, setPrevEpoch] = useState(epochNumber)
-  const [totalCount, setTotalCount] = useState(0)
-
   const [page, setPage] = useManageQueryValue('page', null, toIntOrNull)
-  const {data: pagedDataResult, loading, error} = useLoadData(
-    page == null ? null : page * rowsPerPage,
-    epochNumber
-  )
+  const [cache, setCache] = useState({
+    // Note: we have to start with current epochNumber and dummy loading state
+    // so that this handles page reload correctly
+    epochNumber,
+    loading: true,
+    error: null,
+    blocks: [],
+    totalCount: 0,
+    nextPageCursor: null,
+    effectivePage: 0,
+  })
 
-  const {totalCount: dataTotalCount, blocks} = pagedDataResult || {}
-  const effectivePage = page == null ? Math.ceil(totalCount / rowsPerPage) : page
+  const pageToLoad = page == null || cache.epochNumber !== epochNumber ? null : page
+  // Note(ppershing): this overfetches on the last page.
+  // We might need to cap this to `Math.min(pageToLoad * rowsPerPage, cache.totalCount)`
+  // to have valid cursor (right now the backend does not complain though).
+  // However, capping would be trouble on itself as it would lag displaying new content
+  // by 1 load (first get new total count, only following request would not be capped)
+  // Note that this is only about an explicit selection of the last page,
+  // e.g. user paginating to previous page and then back
+  const cursorToLoad = pageToLoad == null ? null : pageToLoad * rowsPerPage
+  const {data, loading, error} = useLoadData(cursorToLoad, epochNumber)
 
-  useEffect(() => {
-    if (dataTotalCount > totalCount) setTotalCount(dataTotalCount)
-  }, [totalCount, dataTotalCount])
+  const getEffectivePage = (page, totalCount) =>
+    page != null ? page : getPageCount(totalCount, rowsPerPage)
 
-  // Reset values after epoch was changed
-  // TODO: this is hot-fix, lets refactor this part soon as it is fragile
-  useEffect(() => {
-    if (epochNumber !== prevEpoch) {
-      setPrevEpoch(epochNumber)
-      setTotalCount(0)
-      setPage(null)
-    }
-  }, [epochNumber, prevEpoch, loading, pagedDataResult, page, setPage])
-
-  return {
-    page,
-    effectivePage,
-    setPage,
+  // Note: this could be wrong if currently loading
+  const newData = {
+    epochNumber, // keep for cache
     loading,
     error,
-    blocks,
-    totalCount: totalCount || 0,
-    nextPageCursor: pagedDataResult && pagedDataResult.cursor,
+    blocks: data.blocks,
+    totalCount: Math.max(data.totalCount, epochNumber === cache.epochNumber ? cache.totalCount : 0),
+    nextPageCursor: data.cursor,
+    effectivePage: getEffectivePage(pageToLoad, data.totalCount),
   }
+
+  useEffect(() => {
+    if (loading) return
+
+    assert(_.isEqual(Object.keys(cache), Object.keys(newData)))
+
+    if (!_.isEqual(cache, newData)) setCache(newData)
+  }, [loading, cache, newData])
+
+  useEffect(() => {
+    if (page !== pageToLoad) {
+      // synchronize page back to URL
+      setPage(pageToLoad)
+    }
+  })
+
+  return loading
+    ? {
+      ...cache,
+      effectivePage:
+          // If we changed page manually, we want to see it immediatelly on the screen
+          // but if it was changed automatically, we need to wait to avoid double-changes
+          cache.epochNumber === epochNumber
+            ? getEffectivePage(page, cache.totalCount)
+            : cache.effectivePage,
+      setPage,
+    }
+    : {
+      ...newData,
+      setPage,
+    }
 }
 
 const Blocks = ({blocksCount, epochNumber}) => {
