fc6cf7f297afd87491e55cab1d1aad85fb018b78
Richard <richardizip@gmail.com>
2019-07-04 12:06:50 +0200

    Mobile staking settings dialog

--- frontend/src/screens/Staking/SideMenu/index.js
+++ frontend/src/screens/Staking/SideMenu/index.js
@@ -55,7 +55,14 @@ const SideMenu = () => {
   const {autoSync, toggleAutoSync} = useAutoSyncContext()
   const isMobile = useIsMobile()
 
-  if (isMobile) return <NavigationBar />
+  if (isMobile) {
+    return (
+      <React.Fragment>
+        <SettingsBar />
+        <NavigationBar />
+      </React.Fragment>
+    )
+  }
 
   return (
     <Card className={classes.wrapper}>
