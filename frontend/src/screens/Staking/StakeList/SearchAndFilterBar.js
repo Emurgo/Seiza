046452b3efc635736c93633be853c7ec17febb4a
Richard <richardizip@gmail.com>
2019-07-05 15:55:43 +0200

    Mobile staking filters animation

--- frontend/src/screens/Staking/StakeList/SearchAndFilterBar.js
+++ frontend/src/screens/Staking/StakeList/SearchAndFilterBar.js
@@ -1,15 +1,21 @@
 // @flow
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 import React, {useCallback, useState} from 'react'
 import {defineMessages} from 'react-intl'
 import {Grid, Collapse} from '@material-ui/core'
 import {makeStyles} from '@material-ui/styles'
 
 import {useI18n} from '@/i18n/helpers'
-import {Searchbar, ToggleButton} from '@/components/visual'
+import {Searchbar, ToggleButton, Button} from '@/components/visual'
 import {useStateWithChangingDefault} from '@/components/hooks/useStateWithChangingDefault'
+import {useIsMobile} from '@/components/hooks/useBreakpoints'
+
 import Filters from './Filters'
 import {useSearchTextContext} from '../context/searchText'
 import {usePerformanceContext} from '../context/performance'
+
+import {ReactComponent as CloseFilters} from '@/static/assets/icons/close-filter.svg'
+import {ReactComponent as OpenFilters} from '@/static/assets/icons/filter.svg'
 
 const messages = defineMessages({
   searchPlaceholder: 'Search for a Stake Pool by name',
@@ -27,9 +33,6 @@ const useStyles = makeStyles((theme) => ({
   },
   filtersWrapper: {
     marginTop: '20px',
-  },
-  button: {
-    width: '120px',
   },
 }))
 
@@ -68,9 +71,75 @@ const Search = () => {
   )
 }
 
+const useFiltersButtonClasses = makeStyles((theme) => ({
+  enter: {
+    opacity: 0,
+  },
+  enterActive: {
+    opacity: 1,
+    transition: 'opacity 300ms 200ms',
+  },
+  leave: {
+    opacity: 1,
+  },
+  leaveActive: {
+    opacity: 0,
+    transition: 'opacity 300ms',
+  },
+  desktopButton: {
+    width: '120px',
+  },
+  mobileButton: {
+    position: 'relative',
+    // Note: keep in sync with searchbar height
+    // TODO: consider exporting/importing
+    height: 58,
+  },
+  icon: {
+    position: 'absolute',
+    // TODO: can we center both icons without absolute/top/left?
+    top: 18,
+    left: 22,
+  },
+}))
+
+const FiltersButton = ({open, onClick}) => {
+  const classes = useFiltersButtonClasses()
+  const {translate: tr} = useI18n()
+  const isMobile = useIsMobile()
+
+  return isMobile ? (
+    <Button onClick={onClick} secondary className={classes.mobileButton}>
+      <ReactCSSTransitionGroup
+        transitionName={{
+          enter: classes.enter,
+          enterActive: classes.enterActive,
+          leave: classes.leave,
+          leaveActive: classes.leaveActive,
+        }}
+        transitionLeave
+        transitionLeaveTimeout={300}
+        transitionEnter
+        transitionEnterTimeout={500}
+        transitionAppear={false}
+        component="div"
+      >
+        {open ? (
+          <CloseFilters key="opened" className={classes.icon} />
+        ) : (
+          <OpenFilters key="close" className={classes.icon} />
+        )}
+      </ReactCSSTransitionGroup>
+    </Button>
+  ) : (
+    <ToggleButton {...{open, onClick}} secondary className={classes.desktopButton}>
+      {tr(messages.filters)}
+    </ToggleButton>
+  )
+}
+
 export default () => {
   const classes = useStyles()
-  const {translate: tr} = useI18n()
   const [showFilters, onToggleShowFilters] = useToggleFilters()
 
   return (
@@ -80,14 +149,7 @@ export default () => {
           <div className={classes.searchWrapper}>
             <Search />
           </div>
-          <ToggleButton
-            open={!showFilters}
-            onClick={onToggleShowFilters}
-            secondary
-            className={classes.button}
-          >
-            {tr(messages.filters)}
-          </ToggleButton>
+          <FiltersButton open={showFilters} onClick={onToggleShowFilters} />
         </Grid>
       </Grid>
       <Collapse in={showFilters}>
