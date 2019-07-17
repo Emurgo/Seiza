0524eaddae655aaf8f3992093186ed19870e4b34
Richard <richardizip@gmail.com>
2019-07-17 14:07:13 +0200

    Smaller revenue circle on mobile

--- frontend/src/components/common/ResponsiveCircularProgressBar.js
+++ frontend/src/components/common/ResponsiveCircularProgressBar.js
@@ -1,13 +1,22 @@
 // @flow
 import React from 'react'
-import type {ComponentType} from 'react'
+
 import CircularProgressBar from '@/components/visual/CircularProgressBar'
-import type {ExternalProps} from '@/components/visual/CircularProgressBar'
 import {useIsMobile} from '@/components/hooks/useBreakpoints'
+
+import type {ExternalProps} from '@/components/visual/CircularProgressBar'
+import type {ComponentType} from 'react'
 
 const ResponsiveCircularProgressBar: ComponentType<ExternalProps> = (props) => {
   const isMobile = useIsMobile()
-  return <CircularProgressBar size={isMobile ? 75 : 85} {...props} />
+  return (
+    <CircularProgressBar
+      height={isMobile ? 40 : 85}
+      width={isMobile ? 100 : 85}
+      textAlign={isMobile ? 'left' : 'middle'}
+      {...props}
+    />
+  )
 }
 
 export default ResponsiveCircularProgressBar
