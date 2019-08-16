c3a3960c10e8f2e468d60e9c019ade7be1f4dba3
leivaburto <cristobal.leiva@usach.cl>
2019-08-16 18:29:32 +0100

    Adding activecampaign script with Helmet

--- frontend/src/screens/SubscribeConfirmation.js
+++ frontend/src/screens/SubscribeConfirmation.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {Grid, Typography} from '@material-ui/core'
 import {makeStyles} from '@material-ui/styles'
 import {defineMessages} from 'react-intl'
+import {Helmet} from 'react-helmet'
 
 import {useI18n} from '@/i18n/helpers'
 import {useAnalytics} from '@/components/context/googleAnalytics'
@@ -47,6 +48,15 @@ const SubscribeConfirmation = () => {
         {tr(messages.subscriptionConfirmedMessage)}
       </Typography>
       <img src={subscribedImage} alt="" className={classes.image} />
+      <Helmet
+        script={[
+          {
+            type: 'text/javascript',
+            innerHTML:
+              '(function(e,t,o,n,p,r,i){e.visitorGlobalObjectAlias=n;e[e.visitorGlobalObjectAlias]=e[e.visitorGlobalObjectAlias]||function(){(e[e.visitorGlobalObjectAlias].q=e[e.visitorGlobalObjectAlias].q||[]).push(arguments)};e[e.visitorGlobalObjectAlias].l=(new Date).getTime();r=t.createElement("script");r.src=o;r.async=true;i=t.getElementsByTagName("script")[0];i.parentNode.insertBefore(r,i)})(window,document,"https://diffuser-cdn.app-us1.com/diffuser/diffuser.js","vgo"); vgo("setAccount", "799238121"); vgo("setTrackByDefault", true); vgo("process");',
+          },
+        ]}
+      />
     </Grid>
   )
 }
