6878931252804a44c6e7e6db6d6a217efe66fd57
Nicolas Arqueros <nykholas@gmail.com>
2019-12-02 21:05:40 -0500

    yoroi chrome

--- frontend/src/helpers/iframeToYoroiMessaging.js
+++ frontend/src/helpers/iframeToYoroiMessaging.js
@@ -1,5 +1,6 @@
 // @flow
 import React, {useCallback} from 'react'
+import config from '@/config'
 
 const relevantDataForYoroi = (selectedPools) => {
   const pools = _.map(selectedPools, _.partialRight(_.pick, ['name', 'poolHash']))
@@ -9,19 +10,21 @@ const relevantDataForYoroi = (selectedPools) => {
 type SelectedPools = Array<{name: string, poolHash: string}>
 
 export const YoroiCallback = (selectedPools: SelectedPools) => {
+  // TODO: We should pass an enum so we can post the specific message.
+  // the type should come from a parameter in the URL
   return useCallback(() => {
-    // window.parent.postMessage(
-    //     relevantDataForYoroi(selectedPools),
-    //     `chrome-extension://${config.yoroiChromeExtensionHash}/main_window.html#/staking`
-    // )
+    window.parent.postMessage(
+      relevantDataForYoroi(selectedPools),
+      `chrome-extension://${config.yoroiChromeExtensionHash}/main_window.html#/staking`
+    )
     // window.parent.postMessage(
     //     relevantDataForYoroi(selectedPools),
     //     `moz-extension://${config.yoroiFirefoxExtensionHash}/main_window.html#/staking`
     // )
     // window.ReactNativeWebView.postMessage
-    window.parent.postMessage(
-      relevantDataForYoroi(selectedPools),
-      'yoroi://simple-staking/selection'
-    )
+    // window.parent.postMessage(
+    //   relevantDataForYoroi(selectedPools),
+    //   'yoroi://simple-staking/selection'
+    // )
   }, [selectedPools])
 }
